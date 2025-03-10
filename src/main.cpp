ff8ddfb9dd9313afc5cc34ff15e40b7fbc1f7c80
geraldony <onyedikageraldjnr@gmail.com>
2025-03-10 00:44:17 -0400

    Wrote my first LED_Blink Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
 #include <Arduino.h>
 
+/******* The Blinking LED Test Code  */
+ 
+/** A preprocessor directive that creates a text replacement for pin 2 before code runs. It doesn's store any val in memory.
+  * Pre-processor directives should always be written on top before the setup or loop methods. */
+#define LED_PIN 2
+
 
 void setup() {
-  Serial.begin(115200);  // Start serial communication
-}
+  // The setup function runs only once the ESP 32 is powered up
 
-void loop() {
-  Serial.println("ESP32 with PlatformIO is working!");  // Print message to Serial Monitor
-  delay(1000);  // Wait 1 second
-}
-/*// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
+  pinMode(LED_PIN,OUTPUT); // initializing the pin as an output 
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ // This is where the main code goes 
+ 
+ digitalWrite(LED_PIN, HIGH); //writing the LED to be high(ON)
+
+ delay(1000); // Delaying the LED for sometime so I can notice the change
+
+ digitalWrite(LED_PIN, LOW); // Writing the  to  be LED LOW(OFF)
+ 
+ delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-} */
+ 
